e2c1ef28787d62a4128f1d52bc30984b5d962bb2
Diegom03 <diegomartinm03@gmail.com>
2023-03-25 21:32:28 +0100

    Menu 3

--- BookingProyect/app/src/main/java/com/diego/bookingproyect/Menu.java
+++ BookingProyect/app/src/main/java/com/diego/bookingproyect/Menu.java
@@ -2,13 +2,38 @@ package com.diego.bookingproyect;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class Menu extends AppCompatActivity {
+
+
+    Button btn_logout;
+    FirebaseFirestore mFirebase;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        mFirebase = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+
+        btn_logout = findViewById(R.id.logout_button);
+
+        btn_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                finish();
+                startActivity(new Intent(Menu.this, Login.class));
+            }
+        });
     }
 }
